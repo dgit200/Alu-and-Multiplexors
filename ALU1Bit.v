2251dc6f72c77920af5294f3ebbae4dd9cd251dc
Dwight Thomas <dwight.thomas@bison.howard.edu>
2016-01-29 14:44:30 -0500

    ALU1Bit.v: new ALU

---
+++ ALU1Bit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.01.2016 14:12:40
+// Design Name: 
+// Module Name: ALU1Bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU1Bit(
+    input a
+    );
+endmodule
